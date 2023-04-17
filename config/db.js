6aed11b29d985a1c4423e834958e46dc624e2a57
Sparko-Sol <accounts@sparkosol.com>
2023-04-17 11:35:32 +0500

    :hammer: db connection changed

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const connectDB = async () => {
     try {
         mongoose.set('strictQuery', true);
-        const conn = await mongoose.connect(process.env.NODE_ENV === 'development' ? process.env.MONGO_DB_LOCAL : process.env.MONGO_URI, {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
             useUnifiedTopology: true,
             useNewUrlParser: true,
         })
