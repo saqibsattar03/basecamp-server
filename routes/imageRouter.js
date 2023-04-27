bb5cba4758f9f73586506fd17a4d69ce2dc8af2d
Sparko-Sol <accounts@sparkosol.com>
2023-04-27 11:45:48 +0500

    :hammer: s3 integration

---
+++ routes/imageRouter.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const imageRouter = express.Router()
+import {uploadImageFromS3, deleteImageFromS3} from "../controllers/imageController.js";
+import protect from "../middleware/authMiddleware.js";
+
+imageRouter.route('/:bucket').post(protect, uploadImageFromS3);
+
+imageRouter.route('/:bucket/:fileName').delete(protect, deleteImageFromS3);
+
+export default imageRouter;
