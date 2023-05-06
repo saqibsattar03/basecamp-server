c92256d4c9047287c2f2c3cb10d557c8553b0462
Rashid-khabeer <rkhabeer84@gmail.com>
2023-05-06 18:12:27 +0500

    New image route

--- routes/imageRouter.js
+++ routes/imageRouter.js
@@ -4,6 +4,8 @@ const imageRouter = express.Router()
 import {uploadImageFromS3, deleteImageFromS3} from "../controllers/imageController.js";
 import protect from "../middleware/authMiddleware.js";
 
+imageRouter.route('/profile/:bucket').post(uploadImageFromS3);
+
 imageRouter.route('/:bucket').post(protect, uploadImageFromS3);
 
 imageRouter.route('/:bucket/:fileName').delete(protect, deleteImageFromS3);
